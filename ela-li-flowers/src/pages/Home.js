c153e6b846c32d4a799eb5754576d3c3a5ff6503
ofirtamir <ofirtamir1997@gmail.com>
2025-03-22 23:38:36 +0200

    ה

--- ela-li-flowers/src/pages/Home.js
+++ ela-li-flowers/src/pages/Home.js
@@ -3,11 +3,11 @@ import '../styles/Home.css';
 
 function Home() {
   const galleryImages = [
-    "https://images.unsplash.com/photo-1504199360784-8bf7fd57b23d",
+    "/images/image.png",
     "https://images.unsplash.com/photo-1587316745621-3757c7076f7b",
     "https://images.unsplash.com/photo-1509042239860-f550ce710b93",
     "https://images.unsplash.com/photo-1603190287605-e6ade32fa852",
-    "https://images.unsplash.com/photo-1567265833339-875f53ff110c",
+    "/images/image2.png",
     "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
   ];
 
