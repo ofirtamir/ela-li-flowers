3b34f5636b8fa50f4416efc0922b52848b206568
ofirtamir <ofirtamir1997@gmail.com>
2025-04-23 19:14:41 +0300

    החזרתי חזרה

--- ela-li-flowers/src/components/Header.js
+++ ela-li-flowers/src/components/Header.js
@@ -7,17 +7,21 @@ function Header() {
 
   return (
     <header className="header">
+      {/* המבורגר ימין במובייל */}
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         ☰
       </div>
 
+      {/* הלוגו - מוצג רק במובייל */}
       <h1 className="site-title">Ela-Li Flowers</h1>
 
+      {/* תפריט ניווט */}
       <nav className={`nav ${menuOpen ? 'open' : ''}`}>
         <Link to="/">דף הבית</Link>
         <Link to="/products">קטלוג</Link>
         <Link to="/workshops">סדנאות</Link>
         <Link to="/event-design">עיצוב אירועים</Link>
+
         <Link to="/about">אודות</Link>
         <Link to="/contact">צור קשר</Link>
       </nav>
