a6dd090d5381ecaf1ba51555cd19e8fc44fd2ec6
ofirtamir <ofirtamir1997@gmail.com>
2025-05-02 12:27:16 +0300

    טעינה תמונות

--- ela-li-flowers/src/pages/Home.js
+++ ela-li-flowers/src/pages/Home.js
@@ -3,19 +3,18 @@ import '../styles/Home.css';
 
 function Home() {
   const galleryImages = [
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409919/IMG_5813_-_Copy_fq3wzp.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409916/IMG_9027_akts0t.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409926/IMG_5807_qlbuz7.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409918/IMG_5817_hictj6.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409917/IMG_5765_wgrwxa.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409914/IMG_7493_kxcqwt.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745423014/IMG_5360_1_wdqq34.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745423018/IMG_5793_caqxgs.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745423016/IMG_5401_hi0psa.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745409917/IMG_7494_oovzpe.jpg",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745413454/image10_ny1syd.png",
-    "https://res.cloudinary.com/dic1dbce9/image/upload/v1745423015/IMG_5784_-_Copy_2_nz8gec.jpg"
-    
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409919/IMG_5813_-_Copy_fq3wzp.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409916/IMG_9027_akts0t.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409926/IMG_5807_qlbuz7.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409918/IMG_5817_hictj6.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409917/IMG_5765_wgrwxa.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409914/IMG_7493_kxcqwt.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745423014/IMG_5360_1_wdqq34.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745423018/IMG_5793_caqxgs.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745423016/IMG_5401_hi0psa.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409917/IMG_7494_oovzpe.jpg",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745413454/image10_ny1syd.png",
+    "https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745423015/IMG_5784_-_Copy_2_nz8gec.jpg"
   ];
 
   return (
@@ -23,7 +22,7 @@ function Home() {
       <section
         className="hero-section"
         style={{
-          backgroundImage: "url('https://res.cloudinary.com/dic1dbce9/image/upload/v1745409918/IMG_5817_hictj6.jpg')",
+          backgroundImage: "url('https://res.cloudinary.com/dic1dbce9/image/upload/f_auto,q_auto/v1745409918/IMG_5817_hictj6.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat"
@@ -39,8 +38,7 @@ function Home() {
 
       <section className="about-section">
         <h2>קצת על אלה-לי</h2>
-        <p>כלל הזרים נשזרים בעבודת יד ונבחרים בקפידה. מתוך אמונה כי היופי נמצא בטבעיות של הפרחים. כל זר נוצר מתוך השראה ואהבה לפרחים ולצבעים שבטבע, בהתאם לשיטת השזירה הנבחרת ולפריחה העונתית.
-        </p>
+        <p>כלל הזרים נשזרים בעבודת יד ונבחרים בקפידה. מתוך אמונה כי היופי נמצא בטבעיות של הפרחים. כל זר נוצר מתוך השראה ואהבה לפרחים ולצבעים שבטבע, בהתאם לשיטת השזירה הנבחרת ולפריחה העונתית.</p>
         <p>הזרים המיובשים עברו תהליך ייבוש וישארו איתכם לתמיד והזרים הטריים נבחרים בקפידה בהתאם לעונה ולמקום האירוע.</p>
         <p>הפרחים המיובשים של אלה- לי משלבים בין זר פרחים טבעי ואמיתי לבין יצירה שנשארת איתכן.ם לתקופה ממושכת. ניתנת האפשרות לרכוש זרים יחידים או מקבצים בבחירה אישית.</p>
         <p>את כלל הזרים המיושבים ניתן לרכוש ביחידים ובמקבצים וניתנת האפשרות להוסיף חלוקי נחל לשמירה על יציבות הזר.</p>
@@ -50,7 +48,14 @@ function Home() {
         <h3>טעימה מהעבודות שלנו</h3>
         <div className="gallery">
           {galleryImages.map((img, index) => (
-            <img key={index} src={img} alt={`gallery-${index}`} />
+            <img
+              key={index}
+              src={img}
+              alt={`gallery-${index}`}
+              loading="lazy"
+              width="300"
+              height="200"
+            />
           ))}
         </div>
       </section>
